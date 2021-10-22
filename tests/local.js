f9d0fdf748bf0a07b6cc5642c65729e5056e568b
Tuyen Nguyen <trongtuyen96@gmail.com>
2021-10-22 21:34:25 +0700

    Add azure pipelines with docker

--- tests/local.js
+++ tests/local.js
@@ -5,9 +5,9 @@ import http from "k6/http";
 export let options = {
     // Rampup for 10s from 1 to 15, stay at 15, and then down to 0
     stages: [
-        { duration: "10s", target: 15 },
-        { duration: "20s", target: 15 },
-        { duration: "10s", target: 0 }
+        { duration: "2s", target: 15 },
+        { duration: "5s", target: 15 },
+        { duration: "2s", target: 0 }
     ],
     thresholds: {
         "http_req_duration": ["p(95)<250"]
